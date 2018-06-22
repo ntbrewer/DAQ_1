78f8326f98682d8416c7864c49b9d9f5fa43ef21
dmckinno13 <dmckinno@vols.utk.edu>
2018-06-22 15:50:33 -0400

    Updates alarm to add rate of temperature change.

--- kelvin/SendAlarm.py
+++ kelvin/SendAlarm.py
@@ -1,7 +1,7 @@
 import pexpect as pxp
 import time
 
-def getlist():
+def getList():
     inf = open("log/therm-test.log",'r')
     lines = inf.readlines()
     List=[0,0,0,0,0]
@@ -10,32 +10,107 @@ def getlist():
     avg = sum(List)/5
     return(avg)
 
-thresh = 31
-dTthresh = 0.5 # 0.5 C per hour
-dTime = 60 * 5
+def getListTest(j,nMinutes):
+    inf = open("alarmTest1.txt","r")
+    lines = inf.readlines()
+
+    # "dynamic" length for list based on nMinutes. Better way?
+    List = [0]
+    for i in range(1,nMinutes):
+        List.append(0)
+
+    for i in range(0,nMinutes):
+        List[i] = eval(lines[i+j].split()[-2])
+    avg = sum(List)/nMinutes
+    #print(List)
+    return(avg)
+
+def rateChangeTest(j,nMinutes):
+    inf = open("alarmTest1.txt","r")
+    lines = inf.readlines()
+
+    # "dynamic" length for list based on nMinutes. Better way?
+    List1 = [0]
+    List2 = [0]
+    List3 = [0]
+    runAvg = [0]
+    ListTime = [0] # added for testing
+    for i in range(1,nMinutes):
+        List1.append(0)
+        List2.append(0)
+        List3.append(0)
+        runAvg.append(0)
+        ListTime.append(0) # added for testing
+    #print(List1)
+
+    for i in range(0,nMinutes):
+        List1[i] = eval(lines[i+j].split()[-2])
+        List2[i] = eval(lines[i+j].split()[-3])
+        List3[i] = eval(lines[i+j].split()[-4])
+        ListTime[i] = eval(lines[i+j].split()[-5])
+        #print(str(ListTime[i])+"   "+str(List1[i])+"   "+str(List2[i])+"   "+str(List3[i])+"\n" )
+    for i in range(0,nMinutes):
+        runAvg[i] = (List1[i]+List2[i]+List3[i])/3
+    avg = sum(runAvg)/nMinutes
+    #print(avg)
+    return(avg)
+
+nMinutes = 60    # number of minutes to wait between checks
+thresh   = 31    # temperature threshold
+dTthresh = 0.5   # 0.5 C per hour
+#dTime    = 60 * nMinutes # "dynamic" time in seconds
+dTime    = .001  #test time
+
 #cmd = "mutt -s \"MTAS is Warming up " + str(thresh) + "\" brewer.nathant@gmail.com < LNemail.txt"
 #cmd = 'mutt -s \"MTAS is Warming up \" brewer.nathant@gmail.com < LNemail.txt '
 cmd ='\.\/alarm.sh'
 
-x=0
+ouf  = open('kelvin.email.txt','w') #open file for writing 
+err  = 0   #error flag
+send = 0   #send error flag
+j    = 3   #test index counter - not used during running
 
-# Sleep, get first average to initialize avgLast, sleep again, then start loop
+# Sleep, get first average to initialize rAvgLast, sleep again, then start loop
 time.sleep(dTime)
-avgLast = getlist()
+#avgLast = getList()
+rAvgLast = rateChangeTest(j,nMinutes)
 time.sleep(dTime)
 
-while x==0:
-    avg=getlist()
+while err==0:
+    j += 5
+    #avg=getList()
+    avg = getListTest(j,nMinutes)
+    rAvg = rateChangeTest(j,nMinutes)
+    #dT = abs( (avg - avgLast)*(60/dTime) )
+    #dT = abs( (avg - avgLast)*(60/5) )
+    dT = abs( (rAvg - rAvgLast)*(60/nMinutes) )
+    #print("avg, rAvg, and rAvgLast: " + str(avg) + "    " + str(rAvg) + "   " + str(rAvgLast))
+
     if avg>thresh:
-        pxp.run(cmd)
-        x=1
-    dT = abs( (avg - avgLast)/(2 * dTime) )
+        send = 1
+        err = 1
+
     if dT > dTthresh:
+        send = 1
+        err = 2
+
+    if send == 1:
+        if err == 1:
+            errMessage = "Temperature threshold ({} C) was crossed.".format(thresh)
+        if err == 2:
+            errMessage = "Temperature rate of change threshold ({} C/hr) was crossed.".format(dTthresh)
+        ouf.write("-------------------------------------------------------------------\n" +
+                  errMessage + "\n\n" +
+                  "Average Temp at " + str(time.ctime()) + ": {:.2f} C\n\n".format(avg) +
+                  "Rate of Change in Average Temp: {:.2f} C/hr\n".format(dT) +
+                  "-------------------------------------------------------------------\n")
+        ouf.close()
         pxp.run(cmd)
-        x = 1
-    #pxp.time isn't a thing. Switched to time.sleep - DVM
+        send = 0
     time.sleep(dTime)
+    #avgLast = avg
+    rAvgLast = rAvg
     print("---------------------------------------------------------")
-    print("Average Temp in last " + str(dTime) + " seconds: " + str(avg)+" C")
-    print("Rate of Change in Average Temp: " + str(dT)+" C/hr")
+    print("Average Temp at " + str(time.ctime()) + ": {:.2f} C".format(avg))
+    print("Rate of Change in Average Temp: {:.2f} C/hr".format(dT))
     print("---------------------------------------------------------")
--- kelvin/alarm.sh
+++ kelvin/alarm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Emails are getting blocked to UTK addresses. May need to use different addresses.
 #mutt -s "MTAS is warming over 31C" brewer.nathant@gmail.com -c brasco.utk.edu -c rykaczewskik@ornl.gov -c dmckinno@vols.utk.edu < KelvinEmail.txt
-mutt -s "MTAS is warming over 31C" brewer.nathant@gmail.com < KelvinEmail.txt
-#mutt -s "MTAS is warming over 31C" dmckinno@vols.utk.edu < KelvinEmail.txt
+#mutt -s "MTAS is warming over 31C" brewer.nathant@gmail.com < KelvinEmail.txt
+#mutt -s "MTAS is warming over 31C" dmckinno@vols.utk.edu -c darren.mckinnon.13@gmail.com < KelvinEmail.txt
+mutt -s "MTAS ALARM TESTING" darren.mckinnon.13@gmail.com < kelvin.email.txt
